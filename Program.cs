189d75d13ea02bb1d62fdeae832c525a84f3e98c
mothnue <dinocruz434@gmail.com>
2025-06-13 17:12:16 +0100

    Software Structure

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//using Spectre.Console;
+
+//var panel = new Panel("CODING TRACKER")
+//{
+//    Border = BoxBorder.Rounded,
+//};
+
+//AnsiConsole.Write(panel);
+
+
+namespace CodingTrackerDesign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //- Construct Layout in a loop
+            while (true)
+            {
+
+            }
+        }
+    }
+}
