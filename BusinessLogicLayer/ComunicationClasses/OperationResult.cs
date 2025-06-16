f6e8279cb36b8d8deb9fee7f0c4add8c342fd3e5
mothnue <dinocruz434@gmail.com>
2025-06-16 16:09:06 +0100

    This class will be used to easily establish communication between layers

---
+++ BusinessLogicLayer/ComunicationClasses/OperationResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.ComunicationClasses
+{
+    public class OperationResult
+    {
+        public bool success;
+        public string? message;
+
+        public OperationResult(bool success, string? message = null)
+        {
+            this.success = success;
+            this.message = message;
+        }
+    }
+}
