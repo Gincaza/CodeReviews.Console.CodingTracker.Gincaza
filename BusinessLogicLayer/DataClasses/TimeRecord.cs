d2ba8a0b861134f7fb567b7c69a1cee8d492c8e1
mothnue <dinocruz434@gmail.com>
2025-06-16 16:08:26 +0100

    This class will be used to easily view and manipulate information regarding TimeRecord

---
+++ BusinessLogicLayer/DataClasses/TimeRecord.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// this class will be only used to view the data
+
+namespace BusinessLogicLayer.DataClasses
+{
+    public class TimeRecord
+    {
+        public int id;
+        public string startDate;
+        public string endDate;
+        public string allTime;
+
+        public TimeRecord(int id, string startDate, string endDate, string allTime) 
+        {
+            this.id = id;
+            this.startDate = startDate;
+            this.endDate = endDate;
+            this.allTime = allTime;
+        }
+    }
+}
