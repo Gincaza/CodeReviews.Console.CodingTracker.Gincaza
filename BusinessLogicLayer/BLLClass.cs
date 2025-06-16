26cda54a16f0b9555ff402a8a672ec2c4257daa1
mothnue <dinocruz434@gmail.com>
2025-06-16 15:10:49 +0100

    basic functions for noew to the BLL class

---
+++ BusinessLogicLayer/BLLClass.cs
@@ -0,0 +1,34 @@
+﻿namespace BusinessLogicLayer
+{
+    public class BLLClass
+    {
+        //private static DataAccessLayer dataAccess;
+
+
+        public BLLClass(/*DataAccessLayer dataAccess*/) 
+        {
+            //this.dataAccess = dataAccess
+        }
+
+        public void InitializeDatabase()
+        {
+            //this.dataAccess.InitDatabase();
+        }
+        
+        public void AddTimeRecord(string starTime, string endTime)
+        {
+
+        }
+
+        public void SeeTimeRecord() 
+        {
+
+        }
+
+        public void DeleteTimeRecord(int id)
+        {
+
+        }
+
+    }
+}
